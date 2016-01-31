cecf9f92a4e66dff577c33e1bdce80073c44b89a
onyoo <roberto9m9@gmail.com>
2016-01-31 20:09:56 +0100

    Done.

--- loops.js
+++ loops.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function loopExcitement() {
+  var sentences = new Array()
+  for ( var i = 50 ; i < 75 ; i++ ) {
+    sentences.push("i am looping");
+  }
+  return sentences;
+}
+
+function loopingFun() {
+  var i = 10;
+  var num;
+  while ( i < 17 ) {
+    num = i++;
+  }
+  return num;
+}
+
+function alwaysLooping(number) {
+  var love = new Array();
+  do{
+    love.push("JavaScript");
+    ++number;
+  } while ( number < 10 )
+  return love;
+}

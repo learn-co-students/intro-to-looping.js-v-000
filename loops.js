56129ce8d1181a2ec30b0a4b38b241ec2164a9bd
Justin Thomas <jthomas0209@gmail.com>
2016-03-02 11:17:42 -0500

    Done

--- loops.js
+++ loops.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function loopExcitement(){
+  var sentences = [];
+  for (var i = 51; i <= 75; i++){
+    sentences.push("i am looping"); 
+  }
+  return sentences;
+}
+
+function loopingFun(){
+  var i = 10;
+  var num = null;
+  while (i < 17){
+    num = i;
+    i++;
+  }
+  return num;
+}
+
+function alwaysLooping(parameter){
+  var love = [];
+  do{
+    love.push("JavaScript");
+    parameter++;
+  } while(parameter < 10);
+  return love;
+}

eb431c4f36c5cdf84d861332e2f13f7e83894b4f
Kevin McCormack <wittyman37@yahoo.com>
2016-03-06 18:54:36 -0500

    Done.

--- loops.js
+++ loops.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function loopExcitement() {
+  var sentences = [];
+  for (var i = 50; i < 75; i ++) {
+    sentences.push("i am looping");
+  }
+
+  return sentences;
+}
+
+
+function loopingFun() {
+  var i = 10;
+  var num;
+
+  while(i < 17){
+    num = i;
+    i ++;
+  }
+
+  return num;
+}
+
+function alwaysLooping(n) {
+  var love = [];
+  do {
+    love.push("JavaScript");
+    n += 1;
+  } while (n < 10);
+
+  return love;
+}

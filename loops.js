143c2ad9ca597dff3c44e10786031c73dbd7f5ab
zachnewburgh <zach.newburgh@gmail.com>
2016-03-30 10:03:14 -0400

    Done.

--- loops.js
+++ loops.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function loopExcitement() {
+  var sentences = [];
+  
+  for (var i = 50; i < 75; i++) {
+    sentences.push("i am looping");
+  }
+  
+  return sentences;
+}
+
+function loopingFun() {
+  var i = 10;
+  var num = "";
+
+  while  (i < 17) {
+    num = i;
+    i++;
+  }
+
+  return num;
+}
+
+function alwaysLooping(i) {
+  var love = [];
+
+  do {
+    love.push("JavaScript");
+    i++;
+  } while (i < 10);
+
+  return love;
+}

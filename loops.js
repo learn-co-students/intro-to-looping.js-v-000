7ffffc86a229f718de71fe56a5d84cdc57881492
azheng249 <andy.zheng249@gmail.com>
2016-04-23 19:29:21 -0400

    Done.

--- loops.js
+++ loops.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function loopExcitement() {
+  var sentences = [];
+  
+  for (var i = 0; i < 25; i++) {
+    sentences.push("i am looping");
+  }
+
+  return sentences;
+}
+
+function loopingFun() {
+  var i = 10;
+  var num;
+
+  while (i < 17) {
+    num = i;
+    i++;
+  }
+
+  return num;
+}
+
+function alwaysLooping(number) {
+  var love = [];
+
+  do {
+    love.push("JavaScript");
+    number--;
+  } while (0 < number && number <= 10);
+
+  return love;
+}

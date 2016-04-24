61d4d828ca3b26648db0271b257a50b86673a668
Louiefigz <figueroa.aaron@gmail.com>
2016-04-24 14:27:27 -0400

    Done.

--- loops.js
+++ loops.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function loopExcitement(){
+  var sentences = []
+  for (var i = 1; i < 26; i++){
+    sentences.push("i am looping");
+  }
+  return sentences;
+}
+
+function alwaysLooping(num){
+  var love= []
+  do {
+    love.push("JavaScript")
+    num ++;
+  } while (num<10);
+  return love
+}
+
+function loopingFun(){
+  var i = 10;
+  var num;
+  while (i<17){
+    num = i;
+    i++;
+  }
+  return num
+}

09c709a3d35a0f38f0d802a570f74dfef6cf5b09
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-05-03 10:48:51 -0500

    Done.

--- loops.js
+++ loops.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function loopExcitement() {
+  var sentences = [];
+
+  for (var i = 50; i < 75; i++) {
+    sentences.push("i am looping");
+  }
+  return sentences
+};
+
+function loopingFun() {
+  var i = 10 ;
+  var num = "" ;
+
+  while ( i < 17) {
+    num = i ++
+  };
+
+  return num ;
+}
+
+function alwaysLooping(numero) {
+  var love = [] ;
+
+  do {
+   numero += 1;
+   love.push("JavaScript");
+  } while (numero < 10);
+
+  return love
+}

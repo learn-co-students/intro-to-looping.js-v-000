f4610553ff2ad57651979d7d52abfd3fe87fafdf
Brian Holland <brian.holland@proatria.com>
2016-05-03 21:00:24 -0500

    Done.

--- loops.js
+++ loops.js
@@ -1,1 +1,23 @@
 'use strict';
+function loopExcitement(){
+  var output = [];
+  for (var i=0; i<25; i++){
+    output.push("i am looping");
+  };
+  return output;
+};
+
+function loopingFun(){
+  for (var i=12; i<16; i++){
+  }
+  return i;
+};
+
+function alwaysLooping(num) {
+  if (num<10){
+    var ary = new Array(num).fill("JavaScript");
+    return ary;
+  } else {
+    return ["JavaScript"];
+  }
+};

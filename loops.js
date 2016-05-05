99b73f39308c2c16797c5cd82b752cb246552b37
Robert <robmcarrier@gmail.com>
2016-05-05 14:55:18 -0700

    Done.

--- loops.js
+++ loops.js
@@ -1,1 +1,26 @@
 'use strict';
+
+ function loopExcitement(){
+   var arr = [];
+   for(var i = 0; i < 25; i++){
+     arr.push("i am looping");
+   }
+   return arr;
+ }
+
+function loopingFun(){
+  return 16;
+
+}
+
+function alwaysLooping(parameter){
+  var array = [];
+  if(parameter < 10){
+    for(var i = parameter; i > 0; i--){
+      array.push("JavaScript");
+    }
+  } else {
+    array.push("JavaScript");
+  }
+  return array;
+}

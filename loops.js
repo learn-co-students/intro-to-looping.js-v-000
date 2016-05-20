ba90a223a9c0b766dbfcf20c0999794d1bbc1214
sameera91 <sameera.bachoti@gmail.com>
2016-05-20 14:42:24 -0400

    Done.

--- loops.js
+++ loops.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function forLoop(array){
+  for(i = 0; i < 25; i++){
+    if(i == 1){
+      return "I am 1 strange loop."
+    }
+    else{
+      return "I am ${i} strange loops."
+    }
+  }
